91e02e9c380feb53f427ddf5272eed0e6a976c60
jogp10 <jogp10@gmail.com>
2021-11-29 21:21:01 +0000

    Added Tests and modified Doxyfile

--- docs/output/html/search/functions_4.js
+++ docs/output/html/search/functions_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['luggage_42',['Luggage',['../classLuggage.html#a86a6af4fbe0e3918f0c8b627b8bfa6e0',1,'Luggage']]]
+  ['luggage_42',['Luggage',['../classLuggage.html#a1635d12a770246bc545708a883f71e86',1,'Luggage']]]
 ];
